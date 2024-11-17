b64b156484880e5b49bbf4e11a71b6fd074a59dd
mathias.strommen <mathias.strommen@accenture.com>
2024-11-17 23:23:04 +0100

    Add test to check the content of the match. Could probably been done in several steps.

--- src/main/java/com/example/Match.java
+++ src/main/java/com/example/Match.java
@@ -1,4 +1,12 @@
 package com.example;
 
 public class Match {
+
+    public String getHomeTeam() {
+        return null;
+    }
+
+    public String getAwayTeam() {
+        return null;
+    }
 }
--- src/test/java/com/example/ScoreboardTest.java
+++ src/test/java/com/example/ScoreboardTest.java
@@ -16,11 +16,21 @@ class ScoreboardTest {
     }
 
     @Test
-    void startNewMatch_summaryNotEmpty(){
+    void startNewMatch_summaryNotEmpty() {
         Scoreboard scoreboard = new Scoreboard();
         scoreboard.startMatch("homeTeam", "awayTeam");
         List<Match> summary = scoreboard.getSummary();
         Assertions.assertFalse(summary.isEmpty());
+    }
+
+    @Test
+    void startNewMatch_checkSummaryTeamContent() {
+        Scoreboard scoreboard = new Scoreboard();
+        scoreboard.startMatch("homeTeam", "awayTeam");
+        List<Match> summary = scoreboard.getSummary();
+        var match = summary.getFirst();
+        Assertions.assertEquals("homeTeam", match.getHomeTeam());
+        Assertions.assertEquals("awayTeam", match.getAwayTeam());
     }
 
 
