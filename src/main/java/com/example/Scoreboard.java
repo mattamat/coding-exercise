5727db39fd0b7605e1392a82e7b0fcd73b305fdc
mathias.strommen <mathias.strommen@accenture.com>
2024-11-17 23:07:48 +0100

    Create first test to start match with two teams as string, and generate what was needed to run test

--- src/main/java/com/example/Scoreboard.java
+++ src/main/java/com/example/Scoreboard.java
@@ -1,6 +1,14 @@
 package com.example;
 
+import java.util.List;
+
 public class Scoreboard {
 
+    public void startMatch(String homeTeam, String awayTeam) {
+        
+    }
 
+    public List<Match> getSummary() {
+        return null;
+    }
 }
--- src/test/java/com/example/ScoreboardTest.java
+++ src/test/java/com/example/ScoreboardTest.java
@@ -1,11 +1,17 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class ScoreboardTest {
 
     @Test
-    public void testNothing() {
+    public void startNewMatch() {
         Scoreboard scoreboard = new Scoreboard();
+        scoreboard.startMatch("homeTeam", "awayTeam");
+        List<Match> summary = scoreboard.getSummary();
+        Assertions.assertNotNull(summary);
     }
 }
