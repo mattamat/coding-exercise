69b0afde9d627365e436468e751d09dab5d49b7a
mathias.strommen <mathias.strommen@accenture.com>
2024-11-17 23:09:15 +0100

    Fix failing test

--- src/main/java/com/example/Scoreboard.java
+++ src/main/java/com/example/Scoreboard.java
@@ -1,14 +1,17 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Scoreboard {
 
+    private List<Match> summary = new ArrayList<>();
+
     public void startMatch(String homeTeam, String awayTeam) {
-        
+
     }
 
     public List<Match> getSummary() {
-        return null;
+        return summary;
     }
 }
