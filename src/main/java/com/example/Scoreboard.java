66357f9574a8bf8bd432010a98af842211a01de1
mathias.strommen <mathias.strommen@accenture.com>
2024-11-18 11:56:35 +0100

    Some more refactoring

--- src/main/java/com/example/Scoreboard.java
+++ src/main/java/com/example/Scoreboard.java
@@ -19,33 +19,32 @@ public class Scoreboard {
 
     public List<Match> getSummary() {
         return summary.stream()
-                .sorted(Comparator.comparing(Match::getTotalScore))
+                .sorted(Comparator.comparingInt(Match::getTotalScore))
                 .toList()
                 .reversed();
+
     }
 
     public void updateScore(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore) throws Exception {
-        var matchToUpdate  = summary.stream()
-                .filter(isMatchInSummary(homeTeam, awayTeam))
-                .findFirst()
-                .orElseThrow();
-
+        var matchToUpdate = getMatch(homeTeam, awayTeam);
         matchToUpdate.setHomeScore(homeTeamScore);
         matchToUpdate.setAwayScore(awayTeamScore);
-
     }
 
     public void finishMatch(String homeTeam, String awayTeam) {
-        var finishedMatch = summary.stream()
-                .filter(isMatchInSummary(homeTeam, awayTeam))
-                .findFirst()
-                .orElseThrow();
-
+        var finishedMatch = getMatch(homeTeam, awayTeam);
         summary.remove(finishedMatch);
     }
 
     private Predicate<Match> isMatchInSummary(String homeTeam, String awayTeam) {
         return match ->
                 match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam);
+    }
+
+    private Match getMatch(String homeTeam, String awayTeam) {
+        return summary.stream()
+                .filter(isMatchInSummary(homeTeam, awayTeam))
+                .findFirst()
+                .orElseThrow();
     }
 }
